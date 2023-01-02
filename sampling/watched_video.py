f249f015c855594ff7aa34f99c4f15e9db8bfd6c
minliang_mac <minliang.lin@hotstar.com>
2023-01-02 09:12:33 +0800

    Bug of timezone & FloatType fixed.

--- sampling/watched_video.py
+++ sampling/watched_video.py
@@ -1,9 +1,9 @@
 import pyspark.sql.functions as F
-from pyspark.sql.types import BooleanType, ArrayType, TimestampType, StringType
+from pyspark.sql.types import ArrayType, TimestampType, IntegerType
+from pyspark.sql.types import BooleanType, StringType, FloatType
 from functools import reduce
 import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import json
 
 root_path = "s3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/"
@@ -24,30 +24,27 @@ def is_valid_title(title):
             return False
     return ' vs ' in title
 
-@F.udf(returnType=ArrayType(TimestampType()))
-def match(end, watch_time, break_end):
+@F.udf(returnType=FloatType())
+def intersect(end, watch_time, break_start, break_end):
     start = end - pd.Timedelta(seconds=watch_time)
-    return [t for t in break_end if start <= t <= end]
+    sm = 0.0
+    for s, e in zip(break_start, break_end):
+        inter = (min(e, end) - max(s, start)).total_seconds()
+        sm += max(inter, 0)
+    return sm
 
-@F.udf(returnType=ArrayType(StringType()))
-def parse(seg):
+@F.udf(returnType=StringType())
+def parse(segments):
     try:
-        js = json.loads(seg)
+        js = json.loads(segments)
         if type(js) == list:
             for s in js:
                 if s.startswith('SSAI::'):
-                    tag = s
-                    break
+                    return s
         elif type(js) == dict:
-            if 'ssaiTag' in js:
-                tag=js['ssaiTag']
-            else:
-                return []
-        else:
-            return []
-        return tag[6:].split(':')
+            return js.get('ssaiTag')
     except:
-        return []
+        pass
 
 def valid_dates(tournament):
     match_meta_path = "s3://adtech-ml-perf-ads-us-east-1-prod-v1/data/ads_crash/match_meta"
@@ -55,7 +52,6 @@ def valid_dates(tournament):
                     "ipl2022": "TATA IPL 2022",
                     "wc2021": "ICC Men\'s T20 World Cup 2021",
                     "ipl2021": "VIVO IPL 2021"}
-
     match_df = spark.read.parquet(match_meta_path) \
         .where(f'shortsummary="{tournament_dic[tournament]}" and contenttype="SPORT_LIVE"') \
         .where(is_valid_title('title')) \
@@ -65,29 +61,24 @@ def valid_dates(tournament):
                     'shortsummary') \
         .orderBy('date') \
         .distinct()
-    valid_dates = match_df.select('date').distinct().toPandas()['date'] #TODO: this is UTC but playout is IST
-    return valid_dates
+    return match_df.select('date').distinct().toPandas()['date'] #TODO: this is UTC but playout is IST
 
-def main(tournament='wc2022'):
-    test_dates = valid_dates()#[-3:] # small data for test
-    gen = (spark.read.csv(f"{play_out_log_input_path}{d}", header=True) for d in test_dates)
-    playout_df = reduce(lambda x, y: x.union(y), gen).toPandas()
+def load_playout_time(date_col, time_col):
+    ts = (date_col + ' ' + time_col).apply(pd.to_datetime)
+    return pd.Series(ts.dt.tz_localize('Asia/Kolkata').dt.tz_convert(None).dt.to_pydatetime(), dtype=object)
 
-    wt = spark.read.parquet(f'{wt_path}cd=2022-11-13/hr=13/')
-    ext_cols = ['state', 'gender', 'city', 'region', 'pincode', 'device', 'partner_access', 'carrier', 'carrier_hs']
-    wt1 = wt[['dw_p_id', 'content_id', 'watch_time', 'timestamp', 'country',
-        F.expr('lower(language) as language'),
-        F.expr('lower(platform) as platform'),
-        'stream_type', 'play_type', 'content_type', 'user_segments', 'subscription_status']]
-    # wt1 = wt1.where('substring(dw_p_id, 1, 1) == "0"') # 1/16 sampling
-    # wt1 = wt1.replace('FireStick', 'firetv', 'platform').replace('webos', 'web', 'platform')
-
-    playout_df['break_end'] = (playout_df['End Date'] + ' ' + playout_df['End Time']).apply(pd.to_datetime)
-    playout_df['break_end'] = pd.Series(playout_df.break_end.dt.tz_localize('Asia/Kolkata').dt.to_pydatetime(), dtype=object)
+def main():
+    tournament='wc2022'
+    dates = valid_dates(tournament)
+    dt = dates.iloc[-1]
+    playout_df = spark.read.csv(f"{play_out_log_input_path}{dt}", header=True).toPandas()
+    playout_df['break_start'] = load_playout_time(playout_df['Start Date'], playout_df['Start Time'])
+    playout_df['break_end'] = load_playout_time(playout_df['End Date'], playout_df['End Time'])
     playout_gr = playout_df.groupby(['Content ID', 'Playout ID']).aggregate({
         'Language': max,
         'Tenant': max,
         'Platform': lambda s: max(s).split('|'),
+        'break_start': lambda s: sorted(s),
         'break_end': lambda s: sorted(s),
     })
     playout_gr = playout_gr.explode('Platform')
@@ -104,18 +95,25 @@ def main(tournament='wc2022'):
     playout_gr2 = spark.createDataFrame(playout_gr)
     playout_gr3 = playout_gr2.where('platform == "na"').drop('platform')
 
+    wt = spark.read.parquet(f'{wt_path}cd={dt}/hr=13/')
+    # ext_cols = ['state', 'gender', 'city', 'region', 'pincode', 'device',
+    #  'stream_type', 'play_type', 'content_type', 'subscription_status', 'video_position',
+    #  'partner_access', 'carrier', 'carrier_hs']
+    wt1 = wt[['dw_p_id', 'content_id', 'watch_time', 'timestamp', 'country',
+        'stream_type', 'play_type', 'content_type', 'subscription_status',
+        F.expr('lower(language) as language'),
+        F.expr('lower(platform) as platform'),
+        'user_segments']]
     wt2a = wt1.join(playout_gr2, on=['content_id', 'language', 'country', 'platform'])
     wt2b = wt1.join(playout_gr3, on=['content_id', 'language', 'country'])[wt2a.columns] # reorder cols for union
     wt2 =  wt2a.union(wt2b)
-    wt3 = wt2.withColumn('match_break', match('timestamp', 'watch_time', 'break_end')).cache()
-    wt3.write.mode("overwrite").parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_wt3/')
-    wt3 = spark.read.parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_wt3/')
+    wt2.write.mode("overwrite").parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_wt2/')
+    wt3 = wt2.withColumn('ad_time', intersect('timestamp', 'watch_time', 'break_start', 'break_end'))
+    # wt3.write.mode("overwrite").parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_wt3/')
+    # wt3 = spark.read.parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_wt3/')
+    wt4 = wt3.withColumn('cohort', parse('user_segments')) \
+        .groupby('content_id', 'playout_id', 'cohort') \
+        .sum('ad_time').withColumnRenamed('sum(ad_time)', 'inventory')
+    wt4.write.parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_wt4/')
 
-    wt4 = wt3.where('size(match_break) > 0') \
-        .withColumn('match_break', F.explode('match_break')) \
-        .withColumn('cohort', F.explode(parse('user_segments'))) \
-        .groupby('content_id', 'playout_id', 'match_break', 'cohort') \
-        .count()
-    wt4.write.parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_match_cohort_count/')
-    wt5 = spark.read.parquet('s3://hotstar-ads-ml-us-east-1-prod/tmp/minliang/sampling_match_cohort_count/').toPandas()
-
+main()
