ff1db7271d65914d362ce06168e45c9dac9bd5f1
minliang_mac <minliang.lin@hotstar.com>
2023-01-05 00:24:05 +0800

    add top n inventory

--- sampling/analyze_wt.py
+++ sampling/analyze_wt.py
@@ -1,14 +1,17 @@
-import pandas as pd
-import pyspark.sql.functions as F
+N=10
 
-output_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/'
-df=spark.read.parquet(f'{output_path}cohort_agg/')
+# wt_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/cohort_agg/'
+wt_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dw_d_id/cohort_agg/'
+wt = spark.read.parquet(wt_path).toPandas()
+wt2 = wt.groupby(['cd', 'content_id', 'cohort']).sum().reset_index()
+wt_top_ssai = wt2.groupby('cohort').sum()['ad_time'].nlargest(N).index
 
-df2 = df.toPandas()
-df3 = df2.groupby(['cd', 'content_id', 'cohort']).sum().reset_index()
-top_inventory_ssai = 
+cc_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/inventory_concurrency/'
+cc = spark.read.parquet(cc_path).toPandas()
+cc2 = cc.rename(columns={'ssai_tag':'cohort'}) \
+    .groupby(['cd', 'content_id', 'cohort']).sum().reset_index()
 
-print(
-    df3.groupby(['cd', 'content_id']).ad_time.nlargest(3).reset_index(2, drop=True),
-    df3.groupby(['cd', 'content_id']).reach.nlargest(3)
-)
+wt3 = wt2[wt2.cohort.isin(wt_top_ssai)].groupby(['cd', 'cohort']).sum()
+cc3 = cc2[cc2.cohort.isin(wt_top_ssai)].groupby(['cd', 'cohort']).sum()
+final = wt3.join(cc3, how='outer', rsuffix='_cc')
+final.to_csv(f'top{N}.csv')
--- sampling/investigate_join_cost.py
+++ sampling/investigate_join_cost.py
@@ -7,11 +7,4 @@ print(df.count())
 df2 = df.withColumn('break_start', F.expr('explode(break_start)'))
 print(df2.count())
 df2.write.parquet('s3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/testdata/')
-
 df.drop('break_start','break_end').write.parquet('s3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/testdata2/')
-
-df=spark.read.parquet('s3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/cohort_agg_cache_wt/part-00000-e9ac052e-d662-4be9-8d59-52a60ccaf03c-c000.snappy.parquet')
-df.selectExpr('timestamp-make_interval(0,0,0,0,0,0,watch_time)', 'timestamp', 'watch_time').show()
-df.selectExpr('least(0, float(make_interval(0,0,0,0,0,0,watch_time)))', 'watch_time').show()
-df.selectExpr('bigint(timestamp-timestamp)') # work
-df.selectExpr('make_interval(0,0,0,0,0,0,watch_time)', 'watch_time') # not work
