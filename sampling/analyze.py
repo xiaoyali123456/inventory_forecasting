e2aa92251015bf45dd08fe67d5085cb266d1b084
minliang_mac <minliang.lin@hotstar.com>
2023-01-09 19:47:15 +0800

    buggy; very low cosine similarity

--- sampling/analyze.py
+++ sampling/analyze.py
@@ -2,12 +2,17 @@ import numpy as np
 
 wt_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/inventory_wt/cohort_agg/'
 wt = spark.read.parquet(wt_path).toPandas()
-wt2 = wt.groupby(['cd', 'content_id', 'cohort']).sum().reset_index()
-
 cc_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/data/live_ads_inventory_forecasting/sampling/inventory_concurrency/'
-cc = spark.read.parquet(cc_path).toPandas()
-cc2 = cc.rename(columns={'ssai_tag':'cohort'}) \
-    .groupby(['cd', 'content_id', 'cohort']).sum().reset_index()
+cc = spark.read.parquet(cc_path).toPandas().rename(columns={'ssai_tag':'cohort'})
+
+def calc_ratio(df, col):
+    keys = ['cd', 'content_id', 'cohort']
+    df2 = df.groupby(keys).sum().reset_index()
+    df2[col + '_ratio'] = df2[col] / df2.groupby(keys[:2])[col].transform('sum')
+    return df2
+
+cc2 = calc_ratio(cc, 'ad_time')
+wt2 = calc_ratio(wt, 'ad_time')
 
 def save_topn(n=20):
     wt_top_ssai = wt2.groupby('cohort').sum()['ad_time'].nlargest(n).index
@@ -16,9 +21,6 @@ def save_topn(n=20):
     topn = wt3.join(cc3, how='outer', rsuffix='_cc')
     topn.to_csv(f'top{n}.csv')
 
-save_topn()
-
-## END of LOAD DATA
 def select(ssai):
     head = 'SSAI::'
     n = len(head)
@@ -32,30 +34,12 @@ def parse_ssai(df):
     # df2['reach_ratio'] = df2['reach']/df2.groupby(['cd', 'content_id'])['reach'].transform('sum')
     return df2.pivot(index=['cd', 'content_id'], columns='tag', values='ad_time_ratio').fillna(0)
 
-parse_ssai(wt2).to_csv('wc2021_city.csv')
-
-wt4 = wt2.groupby(['cd', 'cohort']).sum()
-cc4 = cc2.groupby(['cd', 'cohort']).sum()
-
-wt4s = wt4.groupby('cd').sum()
-cc4s = cc4.groupby('cd').sum()
-
-wt5 = wt4.join(wt4s, rsuffix='_sum')
-cc5 = cc4.join(cc4s, rsuffix='_sum')
-
-wt5['ad_time_ratio'] = wt5.ad_time/wt5.ad_time_sum
-wt5['reach_ratio'] = wt5.reach/wt5.reach_sum
-cc5['ad_time_ratio'] = cc5.ad_time/cc5.ad_time_sum
-
 def corr(x):
     a=x['ad_time_ratio']
     b=x['ad_time_ratio_cc']
     # a=(a-a.mean())/a.std() # Pearson correlation, doesn't make much difference
     # b=(b-b.mean())/a.std()
     return np.dot(a,b)/np.linalg.norm(a)/np.linalg.norm(b)
-
-wt_cc = wt5.join(cc5, how='outer', rsuffix='_cc').fillna(0.0)
-print(wt_cc.groupby('cd').apply(corr))
 
 def rmse(x):
     a=x['ad_time_ratio']
@@ -69,6 +53,12 @@ def rate_diff(x):
     d = b.apply(lambda x: 1 if x <= 0 else x)
     return np.mean(c/d)
 
-wt_cc = wt5.join(cc5, how='outer', rsuffix='_cc').fillna(0.0)
-print(wt_cc.groupby('cd').apply(rmse))
-print(wt_cc.groupby('cd').apply(rate_diff))
+def metric(wt, cc):
+    wt_cc = pd.merge(wt, cc, on=['cd', 'content_id', 'cohort'], how='outer', suffixes=['','_cc']).fillna(0.0)
+    print(wt_cc.groupby('cd').apply(corr))
+    print(wt_cc.groupby('cd').apply(rmse))
+    print(wt_cc.groupby('cd').apply(rate_diff))
+
+if __name__ == "__main__":
+    metric(wt2, cc2)
+    # parse_ssai(wt2).to_csv('wc2022_city_quarter.csv')
--- sampling/ssai_volatility.py
+++ sampling/ssai_volatility.py
@@ -3,3 +3,8 @@ import pandas as pd
 import pyspark.sql.functions as F
 
 wt_root = 's3://hotstar-dp-datalake-processed-us-east-1-prod/events/watched_video/'
+# aws s3 sync
+tour = 'wc2022.json'
+with open(tour) as f:
+    dates = json.load(f)
+dates = []
