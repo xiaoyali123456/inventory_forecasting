6adbb75a856519f088ca3182ee501de78f318ed6
minliang_mac <minliang.lin@hotstar.com>
2023-01-09 15:17:48 +0800

    refactor: remove debug script.

--- sampling/analyze.py
+++ sampling/analyze.py
@@ -1,8 +1,7 @@
 import numpy as np
 
 N=10
-# wt_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dw_d_id/cohort_agg/'
-wt_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dw_d_id/cohort_agg_quarter/'
+wt_path = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dw_d_id/cohort_agg/'
 wt = spark.read.parquet(wt_path).toPandas()
 wt2 = wt.groupby(['cd', 'content_id', 'cohort']).sum().reset_index()
 wt_top_ssai = wt2.groupby('cohort').sum()['ad_time'].nlargest(N).index
@@ -28,10 +27,11 @@ def parse_ssai(df):
 
 parse_ssai(wt2).to_csv('wc2021_city.csv')
 
+# 
 wt3 = wt2[wt2.cohort.isin(wt_top_ssai)].groupby(['cd', 'cohort']).sum()
 cc3 = cc2[cc2.cohort.isin(wt_top_ssai)].groupby(['cd', 'cohort']).sum()
 topn = wt3.join(cc3, how='outer', rsuffix='_cc')
-# topn.to_csv(f'top{N}.csv')
+topn.to_csv(f'top{N}.csv')
 
 wt4 = wt2.groupby(['cd', 'cohort']).sum()
 cc4 = cc2.groupby(['cd', 'cohort']).sum()
