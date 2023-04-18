daffc0b3ac3b5bfee884f05abd034e4aa0693b5f
sirius <sirius@hotstar.com>
2023-04-18 12:47:37 +0000

    feat: update new match schema

--- pipeline/code/check_new_match.py
+++ pipeline/code/check_new_match.py
@@ -3,6 +3,7 @@ import sys
 
 if __name__ == '__main__':
     cd = sys.argv[1]
+    cd = '2023-04-18'
     store = f's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/cms_match/cd={cd}/'
     matches = spark.sql('''
     SELECT
@@ -12,15 +13,10 @@ if __name__ == '__main__':
         sportsseasonname,
         title,
         LOWER(shortsummary) AS shortsummary,
-        hidden,
-        deleted,
         premium,
         monetisable,
-        replay,
-        highlight,
         languageid,
         freeduration,
-        duration,
         DATE_FORMAT(FROM_UNIXTIME(startdate), 'yyyy-MM-dd') AS startdate,
         DATE_FORMAT(FROM_UNIXTIME(enddate), 'yyyy-MM-dd') AS enddate
     FROM in_cms.match_update_s3
@@ -28,6 +24,10 @@ if __name__ == '__main__':
         contenttype = 'SPORT_LIVE'
         AND LOWER(gamename) = 'cricket'
         AND COALESCE(LOWER(shortsummary) NOT RLIKE 'mock|shadow|test stream|dummy', false)
-        AND duration > 0
+        AND NOT hidden
+        AND NOT deleted
+        AND NOT replay
+        AND NOT highlight
+    ORDER BY startdate DESC
     ''')
-    matches.repartition(1).write.parquet(store)
+    matches.repartition(1).write.mode('overwrite').parquet(store)
