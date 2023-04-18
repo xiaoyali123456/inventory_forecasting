ac31b8e728100fcceeaae3667a1ca6fb6734dfa5
minliang_lin_at_mbp <minliang.lin@hotstar.com>
2023-04-18 20:15:05 +0800

    feat: more new match analysis

--- sampling/new_match_analysis.py
+++ sampling/new_match_analysis.py
@@ -83,3 +83,45 @@ df2[f2('shortsummary')].where('contenttype="SPORT_LIVE" and g2 is null').count()
 df2[f2('shortsummary')].where('contenttype="SPORT_LIVE" and g2 != "cricket"').show()
 print(df2[f2('shortsummary')].where('contenttype="SPORT_LIVE" and g2 != "cricket"').count()) #64
 df2[f2('shortsummary')].where('contenttype="SPORT_LIVE" and g2 != "cricket"')[['shortsummary']].show(10, False)
+
+# Step 4
+matches = spark.sql('''
+    SELECT
+        contentid AS content_id,
+        tournamentid AS tournament_id,
+        sportsseasonid AS sportsseason_id,
+        sportsseasonname,
+        title,
+        LOWER(shortsummary) AS shortsummary,
+        CAST(hidden AS INT) AS hidden,
+        CAST(deleted AS INT) AS deleted,
+        CAST(premium AS INT) AS premium,
+        CAST(vip AS INT) AS vip,
+        CAST(monetisable AS INT) AS monetisable,
+        CAST(replay AS INT) AS replay,
+        CAST(highlight AS INT) AS highlight,
+        languageid,
+        freeduration, duration,
+        DATE_FORMAT(FROM_UNIXTIME(startdate), 'yyyy-MM-dd') AS startdate,
+        DATE_FORMAT(FROM_UNIXTIME(enddate), 'yyyy-MM-dd') AS enddate
+    FROM in_cms.match_update_s3
+    WHERE
+        contenttype = 'SPORT_LIVE'
+        AND LOWER(gamename) = 'cricket'
+        AND COALESCE(LOWER(shortsummary) NOT RLIKE 'mock|shadow|test stream|dummy', false)
+''')
+matches.describe("hidden deleted premium vip monetisable replay highlight languageid freeduration duration".split())
+'''
++-------+--------------------+-------+-------------------+-------------------+-------------------+------+---------+----------+-----------------+------------------+
+|summary|              hidden|deleted|            premium|                vip|        monetisable|replay|highlight|languageid|     freeduration|          duration|
++-------+--------------------+-------+-------------------+-------------------+-------------------+------+---------+----------+-----------------+------------------+
+|  count|                6357|   6357|               6357|               6357|               6357|  6357|     6357|      6357|             6258|              6357|
+|   mean|0.029416391379581564|    0.0|0.20418436369356616|0.20418436369356616| 0.9134812018247601|   0.0|      0.0|      null|256.2607861936721| 77.43102092181847|
+| stddev| 0.16898390244179615|    0.0|0.40313604977284717|0.40313604977284717|0.28115072501557026|   0.0|      0.0|      null|137.5172573458075|1785.6292574408449|
+|    min|                   0|      0|                  0|                  0|                  0|     0|        0|      [10]|                0|                 0|
+|    max|                   1|      0|                  1|                  1|                  1|     0|        0|       [9]|             3300|             42000|
++-------+--------------------+-------+-------------------+-------------------+-------------------+------+---------+----------+-----------------+------------------+
+'''
+
+# hidden => duration == 0
+matches[matches.hidden>0].describe("hidden deleted premium vip monetisable replay highlight languageid freeduration duration".split()).show()
