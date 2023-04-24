9109bb1d87f306f9d3db89033b0e3e0d34c2dc4e
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-04-24 16:40:05 +0800

    arrange codes

--- pipeline/code/test_all.sh
+++ pipeline/code/test_all.sh
@@ -4,7 +4,7 @@ DATE="2023-04-24"
 CODE="s3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/pipeline/code/"
 SPARK="spark-submit --deploy-mode client \
     --packages org.apache.hudi:hudi-spark-bundle_2.11:0.9.0 \
-    --py-files common.py"
+    --py-files common.py,forecasting/path.py,forecasting/config.py,forecasting/util.py"
 
 aws s3 sync $(dirname $0) $CODE
 aws s3 sync $CODE .
