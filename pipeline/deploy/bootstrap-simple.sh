f82372e10730fd1e4bd0bae504d64d7d4028a04c
John Doe <johndoe@example.com>
2023-04-25 09:07:01 +0000

    fix: bootstrap.sh should pip install for all users of EMR

---
+++ pipeline/deploy/bootstrap-simple.sh
@@ -0,0 +1,20 @@
+sudo python3 -m pip install pip==23.1.1
+sudo python3 -m pip install \
+    numpy==1.21.4 \
+    xgboost==1.6.2 \
+    pytz==2020.1 \
+    PyYAML==5.3.1 \
+    jmespath==0.10.0 \
+    click==7.1.2 \
+    boto==2.49.0 \
+    joblib==0.16.0 \
+    tqdm==4.48.2 \
+    pyarrow \
+    s3fs \
+    pandas \
+    holidays \
+    uvicorn \
+    fastapi \
+    scikit-learn \
+    requests \
+    prophet
