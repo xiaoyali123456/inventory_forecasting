7f0eec8ab441293d3c77e02461021a3c8c057391
sirius <sirius@hotstar.com>
2023-05-01 10:17:31 +0000

    feat: update convert.py and preprocess_v2

--- density/convert.py
+++ density/convert.py
@@ -1,7 +1,8 @@
 import pandas as pd
 
-INPUT_PATH = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dense_sparse/qdata_v2/'
-OUTPUT_PATH = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dense_sparse/final/cd=2023-04-28/all.json'
+INPUT_PATH = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dense_sparse/qdata_v3/'
+OUTPUT_PATH = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dense_sparse/final/cd=2023-05-01/all.json'
+OUTPUT_GZ_PATH = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dense_sparse/final/cd=2023-05-02/all.json.gz'
 
 df = pd.read_parquet(INPUT_PATH) # don't use spark due to limit of master result
 df.reach *= 4
@@ -22,34 +23,18 @@ basic = ['platform', 'language', 'city', 'state', 'age', 'device', 'gender']
 df2 = df.groupby(basic).reach.sum().reset_index()
 df2['rank'] = df2.reach.rank(method='first', ascending=False, pct=True)
 df2['density'] = df2['rank'].map(lambda x: classisfy(x, 0.02, 0.2))
-
-convert = {
-    'age': {
-        'other': 'U30',
-        '30+': '30+'
-    },
-    'device': {
-        '':'A_21231588,
-        '':'A_15031263,
-        '':'A_40990869,
-        '':'A_94523754,
-    }
-}
-
-custom_tags = [['', 'C14_1'], ['', 'C15_1']]
-
-for k, v in name_map.items():
-    print(k, v)
-    print(ssai_cfg[ssai_cfg.tagType == v].iloc[0].metaData)
-    break
+custom_tags = pd.DataFrame({'custom_cohort': ['', 'C14_1', 'C15_1', 'C14_1|C15_1']})
 
 df3 = df2.drop(columns='rank')
 df3['requestId'] = 1
 df3['tournamentId'] = 1
 df3['matchId'] = 1
 df3['forecast_version'] = 'mlv1'
-print(df3[:2].to_json(orient='records', indent=2))
+df3['custom_cohort'] =  custom_tags
+df4 = df3.merge(custom_tags, how='cross')
+print(df4.iloc[:2].to_json(orient='records', indent=2))
 
 # final
-df3.to_json('test.json', orient='records')
+# df4.to_json(OUTPUT_PATH, orient='records')
+df4.to_json(OUTPUT_GZ_PATH, orient='records')
 
---
+++ sampling/dense_sparse/preprocess_v2.py
@@ -0,0 +1,125 @@
+import re
+
+import pyspark.sql.functions as F
+from pyspark.sql.types import StringType
+
+output_root = 's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/sampling/dense_sparse/qdata/'
+
+@F.udf(returnType=StringType())
+def nccs(segments):
+    if segments is not None:
+        for x in segments.split('|'):
+            if x.startswith('NCCS_'):
+                return x
+    return 'other'
+
+
+@F.udf(returnType=StringType())
+def gender(segments):
+    if segments is not None:
+        for x in segments.split('|'):
+            if x.startswith('FMD00') or '_FEMALE_' in x:
+                return 'f'
+            if x.startswith('MMD00') or '_MALE_' in x:
+                return 'm'
+    return 'other'
+
+
+@F.udf(returnType=StringType())
+def age(segments):
+    dc = {
+        'FB_MALE_35-44',
+        'FB_MALE_45-54',
+        'FB_MALE_55-64',
+        'FB_MALE_65PLUS',
+        'FB_FEMALE_35-44',
+        'FB_FEMALE_45-54',
+        'FB_FEMALE_55-64',
+        'FB_FEMALE_65PLUS',
+        'FB_BARC_FEMALE_31-40',
+        'FB_BARC_FEMALE_41-50',
+        'FB_BARC_FEMALE_51+',
+        'FB_BARC_MALE_31-40',
+        'FB_BARC_MALE_41-50',
+        'FB_BARC_MALE_51+',
+        'EMAIL_MALE_35-44',
+        'EMAIL_MALE_45-54',
+        'EMAIL_MALE_55-64',
+        'EMAIL_MALE_65PLUS',
+        'EMAIL_FEMALE_35-44',
+        'EMAIL_FEMALE_45-54',
+        'EMAIL_FEMALE_55-64',
+        'EMAIL_FEMALE_65PLUS',
+        'EMAIl_BARC_FEMALE_31-40',
+        'EMAIl_BARC_FEMALE_41-50',
+        'EMAIl_BARC_FEMALE_51+',
+        'EMAIl_BARC_MALE_31-40',
+        'EMAIl_BARC_MALE_41-50',
+        'EMAIl_BARC_MALE_51+',
+        'PHONE_MALE_35-44',
+        'PHONE_MALE_45-54',
+        'PHONE_MALE_55-64',
+        'PHONE_MALE_65+',
+        'PHONE_FEMALE_35-44',
+        'PHONE_FEMALE_45-54',
+        'PHONE_FEMALE_55-64',
+        'PHONE_FEMALE_65+',
+        'PHONE_MALE_TV_31-40',
+        'PHONE_MALE_TV_41-50',
+        'PHONE_MALE_TV_51-60',
+        'PHONE_MALE_TV_60+',
+        'PHONE_FEMALE_TV_31-40',
+        'PHONE_FEMALE_TV_41-50',
+        'PHONE_FEMALE_TV_51-60',
+        'PHONE_FEMALE_TV_60+',
+        'PHONE_BARC_FEMALE_31-40',
+        'PHONE_BARC_FEMALE_41-50',
+        'PHONE_BARC_FEMALE_51+',
+        'PHONE_BARC_MALE_31-40',
+        'PHONE_BARC_MALE_41-50',
+        'PHONE_BARC_MALE_51+',
+        'FMD009V0053599SRMLDESTADS',
+        'MMD009V0053599SRMLDESTADS',
+        'FMD009V0053599HIGHSRMLDESTADS',
+        'MMD009V0053599HIGHSRMLDESTADS',
+    }
+    if segments is not None:
+        for x in segments.split('|'):
+            if x in dc:
+                return '30+'
+    return 'U30'
+
+
+@F.udf(returnType=StringType())
+def device(segments):
+    if segments is not None:
+        dc = {'A_15031263', 'A_94523754', 'A_40990869', 'A_21231588'}
+        for x in segments.split('|'):
+            if x in dc:
+                return x
+    return 'other'
+
+
+prog = re.compile(r"C\d+_[^|]+")
+@F.udf(returnType=StringType())
+def custom(segments):
+    if segments is not None:
+        res = prog.findall(segments)
+        if len(res):
+            return '|'.join(res)
+    return 'other'
+
+
+def postprocess():
+    df = spark.read.parquet(output_root)
+    df2 = df.groupby(
+        'cd', 'is_cricket', 'tournament',
+        'country', 'language', 'platform', 'city', 'state',
+        nccs('segments').alias('nccs'), 
+        device('segments').alias('device'),
+        gender('segments').alias('gender'),
+        age('segments').alias('age'),
+        custom('segments').alias('custom')
+    ).agg(F.sum('watch_time').alias('watch_time'),
+          F.sum('reach').alias('reach'))
+    df2.repartition(8).write.mode('overwrite').parquet(f"{output_root.strip('/')}_v3/")
