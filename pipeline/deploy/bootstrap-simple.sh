21d0ebdc3fc32251a3ffdfe1f13ab9e482451b71
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-05-23 18:29:01 +0800

    change package install order

--- pipeline/deploy/bootstrap-simple.sh
+++ pipeline/deploy/bootstrap-simple.sh
@@ -2,7 +2,6 @@ sudo python3 -m pip install pip==23.1.1
 sudo python3 -m pip install --force-reinstall \
     numpy==1.21.4 \
     xgboost==1.6.2 \
-    holidays \
     pytz==2020.1 \
     PyYAML==5.3.1 \
     jmespath==0.10.0 \
@@ -13,6 +12,7 @@ sudo python3 -m pip install --force-reinstall \
     pandas==1.2.5 \
     pyarrow \
     s3fs \
+    holidays \
     uvicorn \
     fastapi \
     scikit-learn \
