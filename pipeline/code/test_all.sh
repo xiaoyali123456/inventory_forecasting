208c41c44a50efcdf9e08feb964a862f9b23c0b3
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-05-23 17:35:49 +0800

    add comments and optimization for pipeline code

--- pipeline/code/test_all.sh
+++ pipeline/code/test_all.sh
@@ -1,14 +1,14 @@
 set -exu
 
-DATE="2023-04-24"
+DATE="2023-05-22"
 CODE="s3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/pipeline/code/"
 SPARK="spark-submit --deploy-mode client \
     --conf spark.dynamicAllocation.enabled=true \
     --packages org.apache.hudi:hudi-spark-bundle_2.11:0.9.0 \
     --py-files common.py,forecasting/path.py,forecasting/config.py,forecasting/util.py"
 
-aws s3 sync $(dirname $0) $CODE
-aws s3 sync $CODE .
+#aws s3 sync $(dirname $0) $CODE
+#aws s3 sync $CODE .
 
 bash booking/server.sh &
 sleep 5
@@ -21,8 +21,8 @@ sleep 5
 # $SPARK forecasting/active_user_etl_and_predict.py $DATE
 
 # $SPARK forecasting/feature.py $DATE
-# $SPARK forecasting/xgb_model.py $DATE
- $SPARK forecasting/inventory_prediction.py $DATE
+$SPARK forecasting/xgb_model.py $DATE
+$SPARK forecasting/inventory_prediction.py $DATE
 
 #$SPARK sampling/ewma.py $DATE
 
