676faa927d536c24dd4ebb8b7ad0133e4cbc60cb
sirius <sirius@hotstar.com>
2023-06-13 07:48:01 +0000

    feat: add request_id

--- pipeline/code/postprocess.py
+++ pipeline/code/postprocess.py
@@ -1,12 +1,17 @@
 import requests
-from common import BOOKING_TOOL_URL
+import sys
+import pandas as pd
+from common import BOOKING_TOOL_URL, FINAL_ALL_PREDICTION_PATH
 
 if __name__ == '__main__':
-    r = requests.patch(
-        BOOKING_TOOL_URL + 'inventory/123_586/ad-placement/MIDROLL/forecast-request',
-        json = {
-            "request_status": "SUCCESS",
-            "version": 1,
-        }
-    )
-    print('updated status:', r.status_code)
+    DATE = sys.argv[1]
+    df = pd.read_parquet(f'{FINAL_ALL_PREDICTION_PATH}cd={DATE}/')
+    for row in df.itertuples():
+        r = requests.patch(
+            BOOKING_TOOL_URL + f'inventory/{row.request_id}/ad-placement/MIDROLL/forecast-request',
+            json = {
+                "request_status": "SUCCESS",
+                "version": 1,
+            }
+        )
+        print('updated status:', r.status_code)
--- pipeline/code/sampling/combine.py
+++ pipeline/code/sampling/combine.py
@@ -5,12 +5,17 @@ from common import *
 if __name__ == '__main__':
     DATE = sys.argv[1]
     total = pd.read_parquet(f'{TOTAL_INVENTORY_PREDICTION_PATH}cd={DATE}/')
-    ad_time_ratio = pd.read_parquet(f'{AD_TIME_SAMPLING_PATH}cd={DATE}/')
     reach_ratio = pd.read_parquet(f'{REACH_SAMPLING_PATH}cd={DATE}/')
-    ad_time_ratio.ad_time *= total.estimated_inventory.iloc[0]
+    ad_time_ratio = pd.read_parquet(f'{AD_TIME_SAMPLING_PATH}cd={DATE}/')
     ad_time_ratio.rename(columns={'ad_time':'inventory'}, inplace=True)
-    reach_ratio.reach *= total.estimated_reach.iloc[0]
-    ad_time_ratio.to_parquet(f'{FINAL_INVENTORY_PREDICTION_PATH}cd={DATE}/p0.parquet')
-    reach_ratio.to_parquet(f'{FINAL_REACH_PREDICTION_PATH}cd={DATE}/p0.parquet')
-    common_cols = list(set(reach_ratio.columns)&set(ad_time_ratio.columns))
-    ad_time_ratio.merge(reach_ratio, on=common_cols, how='left').to_parquet(f'{FINAL_ALL_PREDICTION_PATH}cd={DATE}/p0.parquet')
+    for i, row in total.iterrows():
+        reach = reach_ratio.copy()
+        inventory = ad_time_ratio.copy()
+        reach.reach *= row.estimated_reach
+        inventory.inventory *= row.estimated_inventory
+        reach.to_parquet(f'{FINAL_REACH_PREDICTION_PATH}cd={DATE}/p{i}.parquet')
+        inventory.to_parquet(f'{FINAL_INVENTORY_PREDICTION_PATH}cd={DATE}/p{i}.parquet')
+        common_cols = list(set(reach.columns)&set(inventory.columns))
+        combine = inventory.merge(reach, on=common_cols, how='left')
+        combine['request_id'] = str(row.request_id)
+        combine.to_parquet(f'{FINAL_ALL_PREDICTION_PATH}cd={DATE}/p{i}.parquet')
