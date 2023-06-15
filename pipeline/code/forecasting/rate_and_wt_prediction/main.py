195acaff54ea5e62f622064c8c42aebbe0fd7b68
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-06-15 13:41:45 +0800

    dubug dnn model in pipeline

--- pipeline/code/forecasting/rate_and_wt_prediction/main.py
+++ pipeline/code/forecasting/rate_and_wt_prediction/main.py
@@ -6,12 +6,12 @@ import sys
 
 def main(DATE):
     train_dataset = pd.read_parquet(f"{pipeline_base_path}/all_features_hots_format_full_avod_and_simple_one_hot/")
-    test_dataset = pd.read_parquet(f"{pipeline_base_path}/prediction/all_features_hots_format_and_simple_one_hot/cd={DATE}/")
+    prediction_dataset = pd.read_parquet(f"{pipeline_base_path}/prediction/all_features_hots_format_and_simple_one_hot/cd={DATE}/")
     for label in label_list:
         print(label)
-        model = LiveMatchRegression(DATE, train_dataset, test_dataset, label)
+        model = LiveMatchRegression(DATE, train_dataset, prediction_dataset, label)
         model.train()
-        model.test()
+        model.prediction()
 
 
 if __name__ == '__main__':
