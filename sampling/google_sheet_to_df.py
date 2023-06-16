329cba37899ca7b6f298af6e866938730038b7d8
sirius <sirius@hotstar.com>
2023-06-16 10:39:36 +0000

    feat: refactor google sheet

--- sampling/google_sheet_to_df.py
+++ sampling/google_sheet_to_df.py
@@ -4,5 +4,5 @@ import pandas as pd
 
 os.system('aws s3 cp s3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/minliang.lin@hotstar.com-service-account.json .')
 gc = gspread.service_account('minliang.lin@hotstar.com-service-account.json')
-x = gc.open('live_inventory_forecasting_cms')
+x = gc.open('Inventory forecast inputs')
 df = pd.DataFrame(x.sheet1.get_all_records())
