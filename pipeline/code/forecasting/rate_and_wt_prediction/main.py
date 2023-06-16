72012cb5e4d22570f3cb06e5114c743b86f3f749
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-06-16 14:09:05 +0800

    dubug dnn model in pipeline

--- pipeline/code/forecasting/rate_and_wt_prediction/main.py
+++ pipeline/code/forecasting/rate_and_wt_prediction/main.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main(DATE):
-    train_dataset = pd.read_parquet(f"{pipeline_base_path}/all_features_hots_format_full_avod_and_simple_one_hot/")
+    train_dataset = pd.read_parquet(f"{pipeline_base_path}/all_features_hots_format_full_avod_and_simple_one_hot_overall/")
     prediction_dataset = pd.read_parquet(f"{pipeline_base_path}/prediction/all_features_hots_format_and_simple_one_hot/cd={DATE}/")
     for label in label_list:
         print(label)
