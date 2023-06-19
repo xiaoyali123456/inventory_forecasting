5a2e47483faa8651b723d3f089fe7750baf13a3c
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-06-19 13:54:28 +0800

    debug config in pipeline

--- pipeline/code/midroll_rate_prediction.sh
+++ pipeline/code/midroll_rate_prediction.sh
@@ -1,6 +1,11 @@
 DATE=$1
 CODE=$2
 
+echo "$DATE"
+
+aws sns publish --topic-arn "arn:aws:sns:us-east-1:253474845919:sirius-notification" --subject "midroll rate forecasting" --message "starts" --region "us-east-1"
+
+
 source activate pytorch
 
 aws s3 sync $CODE .
