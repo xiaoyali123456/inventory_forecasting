2920952c4b6a4b9be6394b827820e64650f8b2ba
sirius <sirius@hotstar.com>
2023-07-06 01:51:41 +0000

    add airflow trigger

--- pipeline/code/postprocess.py
+++ pipeline/code/postprocess.py
@@ -3,6 +3,20 @@ import sys
 import pandas as pd
 from common import BOOKING_TOOL_URL, FINAL_ALL_PREDICTION_PATH
 
+def trigger_airflow(cd):
+    r = requests.post(
+        'https://airflow-prod.data.k8s.hotstar-labs.com/api/experimental/dags/adtech_prod_midroll_inventory_forecast/dag_runs',
+        json={
+            'conf': {
+                'data': {
+                    'ad_placement': 'MIDROLL',
+                    's3_path': f's3://adtech-ml-perf-ads-us-east-1-prod-v1/live_inventory_forecasting/data/final/all_tournament_partition/cd={cd}/'
+                }
+            }
+        }
+    )
+    return r.status_code
+
 if __name__ == '__main__':
     DATE = sys.argv[1]
     df = pd.read_parquet(f'{FINAL_ALL_PREDICTION_PATH}cd={DATE}/')
@@ -15,3 +29,4 @@ if __name__ == '__main__':
             }
         )
         print('updated status:', r.status_code)
+    # trigger_airflow(DATE) # TODO: open this
