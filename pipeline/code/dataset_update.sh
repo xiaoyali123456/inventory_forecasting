58c066d378ce0d36cfea0d3239914eec96ae7cb7
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-07-07 17:15:21 +0800

    fix bug

--- pipeline/code/dataset_update.sh
+++ pipeline/code/dataset_update.sh
@@ -8,5 +8,5 @@ SPARK="spark-submit --deploy-mode client \
     --packages org.apache.hudi:hudi-spark-bundle_2.11:0.9.0 \
     --py-files common.py,config.py,path.py,util.py"
 
-$SPARK midroll/dataset_update/active_user_etl_and_predict.py $DATE
+$SPARK midroll/dataset_update/dau_update.py $DATE
 $SPARK midroll/dataset_update/dataset_update.py $DATE
