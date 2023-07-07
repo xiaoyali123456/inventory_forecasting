112b74ac64d2f1d70243270207d57bef97e4b3bd
sirius <sirius@hotstar.com>
2023-07-07 09:29:02 +0000

    fix raw_playout filter

--- sampling/preroll.py
+++ sampling/preroll.py
@@ -13,6 +13,52 @@ def latest_n_day_matches(cd, n):
     df = matches[['startdate', 'content_id']].distinct().toPandas().sort_values('startdate')
     return df[df.startdate < cd].iloc[-n:]
 
+
+def load_custom_tags(cd: str) -> dict:
+    res = {}
+    for r in load_requests(cd):
+        for x in r.get('customAudiences', []):
+            if 'segmentName' in x:
+                res[x['segmentName']] = x['customCohort']
+    return res
+
+
+@F.udf(returnType=StringType())
+def convert_custom_cohort(long_tags):
+    long_tags.sort()
+    short_tags = [c_tag_dict[i] for i in long_tags if i in c_tag_dict]  # Need drop duplicates?
+    return '|'.join(short_tags)
+
+
+def process_custom_tags(cd):
+    CUSTOM_COHORT_PATH = ''
+    global c_tag_dict
+    c_tag_dict = load_custom_tags(cd)
+    t = s3.glob('hotstar-ads-targeting-us-east-1-prod/adw/user-segment/ap_user_tag/cd*/hr*/segment*/')
+    t2 = s3.glob('hotstar-ads-targeting-us-east-1-prod/adw/user-segment/custom-audience/cd*/hr*/segment*/')
+    f = lambda x: any(x.endswith(c) for c in c_tag_dict)
+    t3 = ['s3://' + x for x in t + t2 if f(x)]
+    if len(t3) > 0:
+        ct = spark.read.parquet(*t3).groupby('dw_d_id').agg(F.collect_set('tag_type').alias('segments')) \
+            .withColumn('segments', convert_custom_cohort('segments'))
+        matches_days = latest_match_days(cd, 5) # TODO: filter 3 month expiration
+        sql = ','.join(f'"{x}"' for x in matches_days)
+        wt = spark.sql(f'select * from {DAU_TABLE} where cd in ({sql})')
+        wt1 = wt[['dw_d_id',
+            F.expr('lower(cms_genre) == "cricket" as is_cricket'),
+            F.expr('case when watch_time < 86400 then watch_time else 0 end as watch_time')
+        ]]
+        res = wt1.join(ct, on='dw_d_id', how='left').groupby('is_cricket', 'segments').agg(
+            F.expr('sum(watch_time) as watch_time'),
+            F.expr('count(distinct dw_d_id) as reach')
+        )
+    else:
+        t = pd.DataFrame([[False, '', 1.0, 1.0]], columns=['is_cricket', 'segments', 'watch_time', 'reach'])
+        res = spark.createDataFrame(t)
+    res.repartition(1).write.mode('overwrite').parquet(f'{CUSTOM_COHORT_PATH}cd={cd}/')
+
+
+
 def process(cd, content_id):
     df = spark.read.parquet(f'{PREROLL_INVENTORARY_AGG_PATH}cd={cd}/')
     df2 = df.where(f"content_id='{content_id}' and lower(ad_placement) = 'preroll'").groupby(
@@ -23,7 +69,7 @@ def process(cd, content_id):
         'location_cluster',
         'pincode',
         F.expr("concat_ws('|', array_sort(ibt_list)) as interest"),
-        # 'custom_audience', # extract from recent table
+        # 'custom_audience', # extract from recent tables
         'device_brand',
         'device_model',
         # 'primary_sim' # problem
@@ -43,7 +89,3 @@ for row in matches.itertuples():
     print(datetime.now(), row.startdate)
     if os.system(f'aws s3 ls {PREROLL_DAILY_RATIO_PATH}cd={row.startdate}/_SUCCESS') != 0:
         process(row.startdate, row.content_id)
-    # # debug
-    # cd, content_id = row.startdate, row.content_id
-    # group_cols = ['cd']
-    # break
