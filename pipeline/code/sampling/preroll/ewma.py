ce11084535ca98f588bd020ba3ec2d2cba74e720
sirius <sirius@hotstar.com>
2023-07-11 09:02:37 +0000

    add moving average for preroll

--- pipeline/code/sampling/preroll/ewma.py
+++ pipeline/code/sampling/preroll/ewma.py
@@ -10,17 +10,13 @@ cohort_cols = ['gender', 'age_bucket', 'city', 'state', 'location_cluster', 'pin
 
 def load_inventory(cd, n=15):
     last_cd = get_last_cd(PREROLL_SAMPLING_PATH, cd, n)
-    lst = [spark.read.parquet(f'{INVENTORY_SAMPLING_PATH}cd={i}').withColumn('cd', F.lit(i)) for i in last_cd]
+    lst = [spark.read.parquet(f'{PREROLL_SAMPLING_PATH}cd={i}').withColumn('cd', F.lit(i)) for i in last_cd]
     return reduce(lambda x,y: x.union(y), lst)
 
 
-def moving_avg(df, group_cols, target, alpha=0.2):
-    df2 = df.fillna('')  # this is important because pandas groupby will ignore null
-    df2[target+'_ratio'] = df2[target] / df2.groupby(group_cols)[target].transform('sum')  # index=cd, cols=country, platform,..., target, target_ratio
-    df3 = df2.pivot_table(index=group_cols, columns=cohort_cols, values=target+'_ratio', aggfunc='sum').fillna(0)  # index=cd, cols=cohort_candidate_combination1, cohort_candidate_combination2, ...
-    # S[n+1] = (1-alpha) * S[n] + alpha * A[n+1]
-    df4 = df3.ewm(alpha=alpha, adjust=False).mean().shift(1)
-    return df4.iloc[-1].rename(target).reset_index()  # cols=country, platform,..., target
+def average(df, group_cols, targets = ['inventory', 'reach']):
+    res = df.groupby(group_cols).agg(*[F.sum(x).alias(x) for x in targets]).toPandas()
+    return res
 
 
 def merge_custom_cohort(df, cd, src_col='watch_time', dst_col='ad_time'):
@@ -36,9 +32,6 @@ def merge_custom_cohort(df, cd, src_col='watch_time', dst_col='ad_time'):
 if __name__ == '__main__':
     DATE = sys.argv[1]
     iv = load_inventory(DATE)
-    df = moving_avg(iv, ['cd'], target='ad_time')
-    ad_time = merge_custom_cohort(df, DATE)
-    df2 = moving_avg(iv, ['cd'], target='reach')
-    reach = merge_custom_cohort(df2, DATE, 'reach', 'reach')
-    final = ad_time.merge(reach, on=['cd'] + cohort_cols)
+    df = average(iv, ['cd'], targets=['ad_time', 'reach'])
+    final = merge_custom_cohort(df, DATE, 'reach', 'reach')
     final.to_parquet(f'{PREROLL_RATIO_RESULT_PATH}cd={DATE}/p0.parquet')
