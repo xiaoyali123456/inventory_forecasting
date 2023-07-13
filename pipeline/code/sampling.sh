8afe064b7df76d3ea870c61869865d639233f401
sirius <sirius@hotstar.com>
2023-07-13 05:59:52 +0000

    fix: remove useless server in sampling

--- pipeline/code/sampling.sh
+++ pipeline/code/sampling.sh
@@ -2,8 +2,6 @@ DATE=$1
 CODE=$2
 aws s3 sync $CODE . --quiet
 
-bash booking/server.sh &
-
 SPARK="spark-submit --deploy-mode client \
     --packages org.apache.hudi:hudi-spark-bundle_2.11:0.9.0 \
     --py-files common.py"
