241fe024ca8aa72cbd0ed803a99b5fd08f153372
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-07-20 14:16:42 +0800

    change vpc

--- pipeline/code/preprocess.sh
+++ pipeline/code/preprocess.sh
@@ -6,6 +6,9 @@ aws s3 sync $CODE . --quiet
 bash booking/server.sh &
 sleep 5
 
+curl http://adtech-inventory-booking-service-alb-0-int.internal.sgp.hotstar.com/api/v1/inventory/forecast-request\?page-size\=10\&page-number\=0
+
+
 SLACK_NOTIFICATION_TOPIC="arn:aws:sns:us-east-1:253474845919:sirius-notification"
 REGION="us-east-1"
 aws sns publish --topic-arn "$SLACK_NOTIFICATION_TOPIC" --subject "midroll inventory forecasting" --message "booking tool server starts" --region $REGION
