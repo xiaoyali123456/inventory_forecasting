32c5c890217b9267a2cecef06428f64e150938c8
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-07-27 14:44:16 +0800

    debug postprocess

--- pipeline/code/postprocess.sh
+++ pipeline/code/postprocess.sh
@@ -5,9 +5,13 @@ CODE=$2
 aws s3 sync $CODE ~/CODE --quiet
 cd ~/CODE
 
+SLACK_NOTIFICATION_TOPIC="arn:aws:sns:us-east-1:253474845919:sirius-notification"
+REGION="us-east-1"
+
 python3 -m pip install --user -r postprocess/requirements.txt
-bash booking/server.sh &
-sleep 5
+#bash booking/server.sh &
+#sleep 5
+aws sns publish --topic-arn "$SLACK_NOTIFICATION_TOPIC" --subject "midroll inventory forecasting" --message "postprocess packages installed done" --region $REGION
 
 
 #curl http://adtech-inventory-booking-service-alb-0-int.internal.sgp.hotstar.com/api/v1/inventory/forecast-request\?page-size\=10\&page-number\=0
@@ -15,7 +19,4 @@ sleep 5
 
 PYTHONPATH="$PWD" python3 postprocess/postprocess.py $DATE
 
-
-SLACK_NOTIFICATION_TOPIC="arn:aws:sns:us-east-1:253474845919:sirius-notification"
-REGION="us-east-1"
 aws sns publish --topic-arn "$SLACK_NOTIFICATION_TOPIC" --subject "midroll inventory forecasting" --message "postprocess done" --region $REGION
