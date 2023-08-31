0d00f2373702da92bda108ded836041c291268e0
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-08-31 11:45:41 +0800

    fix new match update issue

--- pipeline/code/dataset_update.sh
+++ pipeline/code/dataset_update.sh
@@ -7,6 +7,7 @@ aws s3 sync $CODE . --quiet
 SPARK="spark-submit --deploy-mode client \
     --packages org.apache.hudi:hudi-spark-bundle_2.11:0.9.0 \
     --conf spark.driver.memory=8g \
+    --conf spark.driver.maxResultSize=0 \
     --py-files config.py,path.py,util.py"
 
 $SPARK forecasting/dataset_update/dau_update.py $DATE
