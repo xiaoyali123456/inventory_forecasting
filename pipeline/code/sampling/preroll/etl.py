51ceb82de19cbffeee2629ad486ec40fca3c13e8
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-09-18 15:13:21 +0800

    fix preroll sampling path issue

--- pipeline/code/sampling/preroll/etl.py
+++ pipeline/code/sampling/preroll/etl.py
@@ -22,7 +22,7 @@ def should_be_used_season(sport_season_name):
 
 
 def load_new_matches(cd):
-    last_cd = get_last_cd(PREROLL_SAMPLING_PATH+"aa/", cd)
+    last_cd = get_last_cd(PREROLL_SAMPLING_PATH, cd)
     if last_cd is None:
         last_cd = '2022-10-15' # 10-16 is world cup 22
     matches = spark.read.parquet(MATCH_CMS_PATH_TEMPL % cd) \
@@ -111,6 +111,7 @@ def process(cd, content_ids):
 
 def main(cd):
     matches = load_new_matches(cd)
+    print(matches)
     if len(matches):
         for dt in matches.startdate.drop_duplicates():
             content_ids = matches[matches.startdate == dt].content_id.tolist()
