6136b937a170067b873d4560374df786e84ef984
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-10-25 14:09:08 +0800

    simply preroll sampling

--- pipeline/code/sampling_preroll.sh
+++ pipeline/code/sampling_preroll.sh
@@ -10,8 +10,8 @@ SPARK="spark-submit --deploy-mode client \
     --py-files config.py,path.py,util.py"
 
 $SPARK sampling/preroll/etl.py $DATE
-$SPARK sampling/preroll/ewma.py $DATE
-$SPARK sampling/preroll/combine.py $DATE
+#$SPARK sampling/preroll/ewma.py $DATE
+#$SPARK sampling/preroll/combine.py $DATE
 
 SLACK_NOTIFICATION_TOPIC="arn:aws:sns:us-east-1:253474845919:sirius-notification"
 REGION="us-east-1"
