60905f633f68a07d41536e0dff7dd5cdfcd07fe0
huangbingyang2020 <bingyang.huang@hotstar.com>
2023-10-26 17:05:37 +0800

    add post process for dnn model

--- pipeline/code/forecasting/rate_and_wt_regression_model/dnn_regression.py
+++ pipeline/code/forecasting/rate_and_wt_regression_model/dnn_regression.py
@@ -104,7 +104,7 @@ class LiveMatchRegression(object):
                  pd.merge(df, predict_filtered_df[["content_id"]], on='content_id', how='inner')[["content_id", f"estimated_{self.label}"]]])
         else:
             entire_tournament_df = pd.merge(df, predict_filtered_df[["content_id", self.label]], on='content_id', how='inner')[["content_id", f"estimated_{self.label}"]]
-        print(entire_tournament_df)
+        # print(entire_tournament_df)
         t_mean = entire_tournament_df[f"estimated_{self.label}"].mean()
         t_std = entire_tournament_df[f"estimated_{self.label}"].std()
         lower_bound = t_mean - t_std
--- pipeline/code/forecasting/rate_and_wt_regression_model/main.py
+++ pipeline/code/forecasting/rate_and_wt_regression_model/main.py
@@ -39,7 +39,7 @@ def main(run_date):
     prediction_dataset = pd.read_parquet(f"{PREDICTION_MATCH_TABLE_PATH}/cd={run_date}/")
     predict_filtered_df = prediction_dataset[prediction_dataset['tournament_name'].apply(lambda x: x[0] == 'icc cricket world cup')]
     predict_filtered_df = predict_filtered_df[predict_filtered_df['if_contain_india_team'].apply(lambda x: x[0] == '0')]
-    print(len(filtered_df))
+    print(len(predict_filtered_df))
     if run_date >= "2023-10-16":
         train_dataset["tournament_name"] = train_dataset['tournament_name'].apply(lambda x:
                                           ["world cup" if "world cup" in a
