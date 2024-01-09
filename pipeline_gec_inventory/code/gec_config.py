3641425ed3cbe9c07c3ec07fcbe031261792a6e2
huangbingyang2020 <bingyang.huang@hotstar.com>
2024-01-09 15:13:53 +0800

    code refactor

--- pipeline_gec_inventory/code/gec_config.py
+++ pipeline_gec_inventory/code/gec_config.py
@@ -1,16 +1,31 @@
 # for gec
 MAX_INT = 2147483647
 VALID_SAMPLE_TAG = 1
-SAMPLING_COLS = ['content_id', 'city', 'state', 'location_cluster', 'pincode',
-                 'gender', 'age_bucket', 'ibt', 'device_brand', 'device_model', 'device_carrier', 'device_network_data',
-                 'user_account_type', 'device_platform', 'device_os_version', 'device_app_version',
-                 'ad_placement', 'content_type', 'content_language', 'demo_age_range', 'custom_tags', 'user_segment',
-                 'show_id', 'genre', 'season_no', 'title', 'channel', 'premium']
+SAMPLING_COLS = ['city',
+                 'state',
+                 'location_cluster',
+                 'pincode',
+                 'demo_gender'
+                 'demo_age_range',
+                 'demo_source',
+                 'ibt',
+                 'device_brand',
+                 'device_model',
+                 'device_carrier', 'device_network_data',
+                 'user_account_type',
+                 'device_platform',
+                 'device_os_version',
+                 'device_app_version',
+                 'ad_placement', 'content_id', 'content_type', 'content_language',
+                 'request_id', 'break_id',
+                 'show_id', 'genre',
+                 'title',
+                 'channel', 'premium',
+                 'custom_tags', 'user_segment']
 VOD_SAMPLING_COLS = ['content_id', 'adv_id', 'city', 'state', 'location_cluster', 'gender', 'age_bucket',
-            'ibt', 'device_brand', 'device_model', 'device_carrier', 'user_account_type',
-            'device_platform', 'ad_placement', 'content_type', 'content_language', 'break_slot_count',
-            'show_id', 'genre', 'season_no', 'channel', 'premium', 'nccs', 'device_price', '3rd_party_cohorts']
-
+                     'ibt', 'device_brand', 'device_model', 'device_carrier', 'user_account_type',
+                     'device_platform', 'ad_placement', 'content_type', 'content_language', 'break_slot_count',
+                     'show_id', 'genre', 'season_no', 'channel', 'premium', 'nccs', 'device_price', '3rd_party_cohorts']
 
 BACKUP_SAMPLE_RATE = 0.25
 ALL_ADPLACEMENT_SAMPLE_BUCKET = 100
@@ -25,4 +40,3 @@ SLACK_NOTIFICATION_TOPIC = "arn:aws:sns:us-east-1:253474845919:sirius-notificati
 REGION = "us-east-1"
 
 # git clone git@github.com:hotstar/live-ads-inventory-forecasting-ml.git
-
