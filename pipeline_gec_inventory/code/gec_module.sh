01a49a0a161f3f147f0c84b60f9ef9a64ae22bef
huangbingyang2020 <bingyang.huang@hotstar.com>
2024-01-17 11:02:44 +0800

    add sort after repartition

--- pipeline_gec_inventory/code/gec_module.sh
+++ pipeline_gec_inventory/code/gec_module.sh
@@ -10,8 +10,8 @@ SPARK="spark-submit --deploy-mode client \
     --conf spark.driver.maxResultSize=0 \
     --py-files gec_config.py,gec_path.py,gec_util.py"
 
-#$SPARK sampling/sampling.py $DATE
-#$SPARK index_building/index_building.py $DATE
+$SPARK sampling/sampling.py $DATE
+$SPARK index_building/index_building.py $DATE
 
 $SPARK inventory_prediction/prediction.py $DATE
 
